bda2fb1a7b02f67e26d3373cac40ec9c741e04a7
lucas <lucas@asia.edu.tw>
2017-09-18 17:07:35 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main
+{
+
+    public static void main(String[] args)
+    {
+	  System.out.println("My first java code.");
+	  for(int cnt=1;cnt<=10;cnt++)
+        {
+            System.out.println("cnt=" + cnt);
+        }
+    }
+}
